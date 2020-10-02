bbca448413a60a04ba2fca256ace4909b4cf25f3
Rahix <rahix@rahix.de>
2020-10-02 11:37:51 +0200

    Add a --version option
    Signed-off-by: Rahix <rahix@rahix.de>

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,16 @@ pub mod elementext;
 pub mod svd;
 pub mod util;
 
-pub use gumdrop::Options;
 pub use elementext::ElementExt;
 pub use error::{DisplayError, Error, Result};
+pub use gumdrop::Options;
 
 #[derive(Debug, Options)]
 /// A tool to convert AVR chip description files (.atdf) to SVD.
 struct Atdf2SvdOptions {
     /// Path to the .atdf file to convert
-    #[options(free, required)]
-    atdf_path: std::path::PathBuf,
+    #[options(free)]
+    atdf_path: Option<std::path::PathBuf>,
 
     /// [optional] Path where to save the SVD file
     #[options(free)]
@@ -30,15 +30,28 @@ struct Atdf2SvdOptions {
     verbose: bool,
 
     help: bool,
+
+    #[options(short = "V", long = "version")]
+    version: bool,
 }
 
 fn main() {
     let args = Atdf2SvdOptions::parse_args_default_or_exit();
 
+    if args.version {
+        println!("atdf2svd v{}", env!("CARGO_PKG_VERSION"));
+        return;
+    }
+
     cli::setup(args.verbose);
 
-    let atdf_file =
-        std::fs::File::open(args.atdf_path).unwrap_or_else(|e| cli::exit_with_error(e.into()));
+    let atdf_file = if let Some(atdf_path) = args.atdf_path {
+        std::fs::File::open(atdf_path).unwrap_or_else(|e| cli::exit_with_error(e.into()))
+    } else {
+        log::error!("Missing atdf-file argument");
+        std::process::exit(1);
+    };
+
     let svd_file: Box<dyn std::io::Write> = if let Some(p) = args.svd_path {
         Box::new(std::fs::File::create(p).unwrap_or_else(|e| cli::exit_with_error(e.into())))
     } else {
