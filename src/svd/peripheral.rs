b0b4dd59b9404be4197d1a24997ab974133fd8d6
Rahix <rahix@rahix.de>
2021-06-09 19:30:30 +0200

    svd: peripheral: Create <addressBlock> elements
    As mandated by the SVD specification, create <addressBlock> elements for
    each continuous memory range which uniquely belongs to this periperal.
    
    Ref: #25

--- src/svd/peripheral.rs
+++ src/svd/peripheral.rs
@@ -2,6 +2,37 @@ use crate::chip;
 use crate::svd;
 use crate::ElementExt;
 
+fn create_address_blocks(p: &chip::Peripheral, el: &mut xmltree::Element) -> crate::Result<()> {
+    let mut registers: Vec<_> = p.registers.values().collect();
+    registers.sort_by(|a, b| a.address.cmp(&b.address));
+    let base = p.base_address().expect("no base address");
+
+    let mut add_address_block = |offset, size| {
+        let mut address_block = xmltree::Element::new("addressBlock");
+        address_block.child_with_text("offset", format!("0x{:X}", offset - base));
+        address_block.child_with_text("size", format!("0x{:X}", size));
+        address_block.child_with_text("usage", "registers");
+        el.children.push(address_block);
+    };
+
+    let mut current_offset = registers[0].address;
+    let mut current_size = 0;
+    for reg in registers.into_iter() {
+        let current_address = current_offset + current_size;
+        if current_address == reg.address {
+            current_size += reg.size;
+        } else {
+            add_address_block(current_offset, current_size);
+
+            current_offset = reg.address;
+            current_size = reg.size;
+        }
+    }
+    add_address_block(current_offset, current_size);
+
+    Ok(())
+}
+
 pub fn generate(p: &chip::Peripheral) -> crate::Result<xmltree::Element> {
     let mut el = xmltree::Element::new("peripheral");
     let base = p.base_address().expect("todo error");
@@ -17,6 +48,8 @@ pub fn generate(p: &chip::Peripheral) -> crate::Result<xmltree::Element> {
         },
     );
     el.child_with_text("baseAddress", format!("0x{:X}", base));
+
+    create_address_blocks(p, &mut el)?;
 
     let mut registers = xmltree::Element::new("registers");
 
