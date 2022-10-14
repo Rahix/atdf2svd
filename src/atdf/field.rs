Rahix/atdf2svd

commit 62e5b6051951077d9391212a726854194bc83471

Drop invalid enumerated values with a warning
Ensure that atdf2svd never emits enumerated values which do not fit into
the fields they are meant for.  If the ATDF file contains such values,
remove them and show a warning to the user.

Fixes: #13
Co-authored-by: Rahix <rahix@rahix.de>

src/atdf/field.rs
@@ -32,7 +32,18 @@ pub fn parse(
                 bitfield_el,
             )
         })?;
-        chip::ValueRestriction::Enumerated(values.clone())
+        let mask_as_int = util::parse_int(mask)?;
+        let mask_as_int = mask_as_int >> mask_as_int.trailing_zeros();
+        let filtered_values: std::collections::BTreeMap<_, _> = values
+            .iter()
+            .filter(|(_, ev)| ev.value & mask_as_int == ev.value)
+            .map(|(k, v)| (k.clone(), v.clone()))
+            .collect();
+
+        if values.len() != filtered_values.len() {
+            log::warn!("Invalid enumerated values dropped for field {}", name);
+        }
+        chip::ValueRestriction::Enumerated(filtered_values)
     } else if unsafe_range {
         chip::ValueRestriction::Unsafe
     } else {

commit 37ca8b307224cb631d40e90bbb489c75a4cbf724

Ensure we don't generate empty enumerated values
Make sure we never generate `ValueRestriction::Enumerated` for an empty
list of enumerated values.  This will become important when switching to
svd-rs as it will refuse accepting such value restrictions.

So far, this situation was only observed for fields where we filter out
invalid enumerated values.  But the check will also cover hypothetical
ATDF files which have an empty list of enumerated values for some field.

Fixes: 62e5b6051951 ("Drop invalid enumerated values with a warning")

src/atdf/field.rs
@@ -43,7 +43,12 @@ pub fn parse(
         if values.len() != filtered_values.len() {
             log::warn!("Invalid enumerated values dropped for field {}", name);
         }
-        chip::ValueRestriction::Enumerated(filtered_values)
+        if !filtered_values.is_empty() {
+            chip::ValueRestriction::Enumerated(filtered_values)
+        } else {
+            log::warn!("Empty enumerated values for field {}", name);
+            chip::ValueRestriction::Unsafe
+        }
     } else if unsafe_range {
         chip::ValueRestriction::Unsafe
     } else {