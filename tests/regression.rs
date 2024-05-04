22334d3e18f50c312b813f5a163ee38bd67dce89
Rahix <rahix@rahix.de>
2024-05-04 21:53:49 +0200

    tests: Use new insta macros
    assert_display_snapshot!() was deprecated, switch to its replacement
    instead.

--- tests/regression.rs
+++ tests/regression.rs
@@ -2,19 +2,19 @@
 fn atmega328p() {
     let mut atdf = std::fs::File::open("tests/atmega328p.atdf").unwrap();
     let svd = atdf2svd::run_test(&mut atdf, vec![]);
-    insta::assert_display_snapshot!(svd);
+    insta::assert_snapshot!(svd);
 }
 
 #[test]
 fn atmega128rfa1() {
     let mut atdf = std::fs::File::open("tests/atmega128rfa1.atdf").unwrap();
     let svd = atdf2svd::run_test(&mut atdf, vec![]);
-    insta::assert_display_snapshot!(svd);
+    insta::assert_snapshot!(svd);
 }
 
 #[test]
 fn attiny817() {
     let mut atdf = std::fs::File::open("tests/attiny817.atdf").unwrap();
     let svd = atdf2svd::run_test(&mut atdf, vec![]);
-    insta::assert_display_snapshot!(svd);
+    insta::assert_snapshot!(svd);
 }
