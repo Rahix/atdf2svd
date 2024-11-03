d2a13604b86791de921e0599f961006a2b1c3e3d
Florian Bezannier <florian.bezannier@hotmail.fr>
2024-11-03 12:31:19 +0100

    refacto: simplify code

--- src/atdf/register.rs
+++ src/atdf/register.rs
@@ -4,6 +4,19 @@ use crate::util;
 use crate::ElementExt;
 use std::collections::BTreeMap;
 
+fn field_map_from_bitfield_children(
+    el: &xmltree::Element,
+    values: &atdf::values::ValueGroups,
+    mode_name: Option<&str>,
+) -> crate::Result<BTreeMap<String, chip::Field>> {
+    el.children
+        .iter()
+        .filter_map(|node| node.as_element().filter(|e| e.name == "bitfield"))
+        .map(|e| atdf::field::parse(e, values, mode_name))
+        .map(|r| r.map(|f| (f.name.clone(), f)))
+        .collect::<Result<BTreeMap<_, _>, _>>()
+}
+
 pub fn parse(
     el: &xmltree::Element,
     offset: usize,
@@ -35,32 +48,27 @@ pub fn parse(
         chip::AccessMode::ReadWrite
     };
 
-    let mut fields: BTreeMap<String, chip::Field> = el
-        .children
-        .iter()
-        .filter_map(|node| node.as_element().filter(|e| e.name == "bitfield"))
-        .map(|e| atdf::field::parse(e, values, None))
-        .map(|r| r.map(|f| (f.name.clone(), f)))
-        .collect::<Result<BTreeMap<_, _>, _>>()?;
+    // get bitfield under register
+    let mut fields: BTreeMap<String, chip::Field> =
+        field_map_from_bitfield_children(el, values, None)?;
 
-    let modes = el
-        .children
+    // get bitfield under register.mode
+    el.children
         .iter()
-        .filter_map(|mode| mode.as_element().filter(|m| m.name == "mode"))
-        .into_iter();
-    for mode in modes {
-        if let Some(mode_name) = mode.attributes.get("name").cloned() {
-            let res = mode
-                .children
-                .clone()
-                .into_iter()
-                .filter_map(|el| el.as_element().filter(|e| e.name == "bitfield").cloned())
-                .map(|e| atdf::field::parse(&e, values, Some(mode_name.clone())))
-                .map(|r| r.map(|f| (f.name.clone(), f)))
-                .collect::<Result<BTreeMap<_, _>, _>>()?;
-            fields.append(&mut res.clone());
-        }
-    }
+        .filter_map(|mode| {
+            mode.as_element()
+                .filter(|m| m.name == "mode")
+                .and_then(|m| m.attributes.get("name").map(|mode_name| (mode_name, m)))
+        })
+        .try_for_each(|(mode_name, el)| {
+            fields.append(&mut field_map_from_bitfield_children(
+                el,
+                values,
+                Some(mode_name),
+            )?);
+            crate::Result::Ok(())
+        })?;
+
     Ok(chip::Register {
         name,
         description,
