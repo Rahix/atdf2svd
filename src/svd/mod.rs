0118cc9987970b5c6df9fad592138a2f6ae25c5b
Rahix <rahix@rahix.de>
2025-12-09 13:32:00 +0100

    Fix undetected partial writes into the SVD file
    `.write()` may only perform a partial write and then returns the number
    of bytes written.  Use `.write_all()` instead which blocks until
    everything was written.

--- src/svd/mod.rs
+++ src/svd/mod.rs
@@ -8,7 +8,7 @@ pub mod restriction;
 pub fn generate<W: std::io::Write>(c: &crate::chip::Chip, mut w: W) -> crate::Result<()> {
     let device = chip::generate(c)?;
     let svd_xml = svd_encoder::encode(&device)?;
-    w.write(svd_xml.as_bytes())?;
+    w.write_all(svd_xml.as_bytes())?;
 
     Ok(())
 }
